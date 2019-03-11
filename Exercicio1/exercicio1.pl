a96344fe8ba0e8ecb53238d9859c0d1c1e8822d5
Joao <joaovieira_96@hotmail.com>
2019-03-11 14:34:58 +0000

    1st commit

--- Exercicio1/exercicio1.pl
+++ Exercicio1/exercicio1.pl
@@ -24,8 +24,31 @@
 % Base de conhecimento com informação sobre cuidado prestado, ato médico , utente
 
 
-:- dynamic(utente/6).
-:- dynamic(servicos/4).
-:- dynamic(consultas/4).
+:- dynamic(utente/4).
+:- dynamic(servico/4).
+:- dynamic(consulta/4).
 
-servicos(1,'desc','inst','cid').
+%--------------------------------------------------------------------------------------------
+% Extensao do predicado utente: #IdUt, Nome, Idade, Cidade -> {V,F}
+
+utente(1,'Pedro',60,'Braga').
+utente(2,'joe',50,'Braga').
+
+
+%--------------------------------------------------------------------------------------------
+% Extensao do predicado servicos: #IdServ, Descricao, Instituicao, Cidade -> {V,F}
+
+servico(1,'Maternidade','Hospital de Braga','Braga').
+
+
+%--------------------------------------------------------------------------------------------
+% Extensao do predicado consultas: Data, #IdUt, #IdServ, Custo -> {V,F}
+
+consulta('2018-12-10',1,1,60.50).
+
+%--------------------------------------------------------------------------------------------
+% Extensao do predicado que realiza a procura de conhecimento
+
+solutions(X,Y,Z) :- findall(X,Y,Z).
+
+
