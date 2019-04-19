7680df9928995ab103ed9148c628844805d0380a
Joao <joaovieira_96@hotmail.com>
2019-04-19 05:13:28 +0100

    declarar incerto e interdito

--- Exercicio2/exercicio2.pl
+++ Exercicio2/exercicio2.pl
@@ -24,8 +24,10 @@
 :- dynamic servico/4.
 :- dynamic consulta/5.
 :- dynamic medico/2.
-:- dynamic '-'/1.
+:- dynamic (-)/1.
 :- dynamic excecao/1.
+:- dynamic nulo/1.
+:- dynamic (::)/2.
 
 % -------------------------------------------------------------------------------------------
 % BASE DE CONHECIMENTO (informação sobre utentes, servicos, medicos e consultas)
@@ -34,64 +36,64 @@
 %--------------------------------------------------------------------------------------------
 % Extensao do predicado utente: #IdUt, Nome, Idade, Cidade -> {V,F,D}
 
-utente(3,'Jose Maria Araújo',45,'Braga').
-utente(4,'Maria dos Santos',12,'Vieira do Minho').
-utente(7,'Marta Santos',55,'Lisboa').
-utente(9,'Joao Pereira',22,'Lisboa').
-utente(10,'Diogo Soares',18,'Lisboa').
-utente(11,'Rita Oliveira',70,'Porto').
-utente(12,'Ana Rita Sousa',43,'Porto').
-utente(13,'Beatriz Cunha',22,'Porto').
-utente(15,'Augusto da Silva',44,'Faro').
+utente(3,"Jose Maria Araújo",45,"Braga").
+utente(4,"Maria dos Santos",12,"Vieira do Minho").
+utente(7,"Marta Santos",55,"Lisboa").
+utente(9,"Joao Pereira",22,"Lisboa").
+utente(10,"Diogo Soares",18,"Lisboa").
+utente(11,"Rita Oliveira",70,"Porto").
+utente(12,"Ana Rita Sousa",43,"Porto").
+utente(13,"Beatriz Cunha",22,"Porto").
+utente(15,"Augusto da Silva",44,"Faro").
 
 %--------------------------------------------------------------------------------------------
 % Extensao do predicado servico: #IdServ, Descricao, Instituicao, Cidade -> {V,F,D}
 
-servico(1,'Cardiologia','Hospital de Braga','Braga').
-servico(2,'Pediatria','Hospital Privado de Braga','Braga').
-servico(3,'Urgencia','Hospital de Braga','Braga').
-servico(5,'Oncologia','IPO','Porto').
-servico(7,'Maternidade','Hospital de Braga','Braga').
-servico(8,'Neurologia','Centro Hospitalar Sao Joao','Porto').
-servico(9,'Oftalmologia','Hospital de Braga','Braga').
-servico(12,'Otorrinolaringologia','Hospital da Luz','Lisboa').
+servico(1,"Cardiologia","Hospital de Braga","Braga").
+servico(2,"Pediatria","Hospital Privado de Braga","Braga").
+servico(3,"Urgencia","Hospital de Braga","Braga").
+servico(5,"Oncologia","IPO","Porto").
+servico(7,"Maternidade","Hospital de Braga","Braga").
+servico(8,"Neurologia","Centro Hospitalar Sao Joao","Porto").
+servico(9,"Oftalmologia","Hospital de Braga","Braga").
+servico(12,"Otorrinolaringologia","Hospital da Luz","Lisboa").
 
 %--------------------------------------------------------------------------------------------
 % [EXTRA] Extensao do predicado medico: #IdMed, Nome -> {V,F,D}
 
-medico(1,'Pedro Araujo').
-medico(2,'Adriana Goncalves').
-medico(3,'Sara Pereira').
-medico(4,'Joao Leal').
-medico(5,'Eduardo Semanas').
-medico(6,'Pedro Almeida').
-medico(7,'Renato Cruzinha').
-medico(8,'Manuel Monteiro').
-medico(9,'Diogo Soares').
-medico(10,'Joao Vieira').
-medico(11,'Bruno Ferreira').
-medico(12,'Frederico Pinto').
-medico(13,'Filipe Fortunato').
-medico(14,'Jorge Costeira').
+medico(1,"Pedro Araujo").
+medico(2,"Adriana Goncalves").
+medico(3,"Sara Pereira").
+medico(4,"Joao Leal").
+medico(5,"Eduardo Semanas").
+medico(6,"Pedro Almeida").
+medico(7,"Renato Cruzinha").
+medico(8,"Manuel Monteiro").
+medico(9,"Diogo Soares").
+medico(10,"Joao Vieira").
+medico(11,"Bruno Ferreira").
+medico(12,"Frederico Pinto").
+medico(13,"Filipe Fortunato").
+medico(14,"Jorge Costeira").
 
 %--------------------------------------------------------------------------------------------
 % Extensao do predicado consulta: Data, #IdUt, #IdServ, Custo, #IdMed[EXTRA] -> {V,F,D}
 
-consulta('2012-3-12',1,1,55.0,4).
-consulta('2012-10-1',2,1,55.0,5).
-consulta('2014-1-18',5,6,0.0,12).
-consulta('2014-2-7',9,12,20.50,6).
-consulta('2015-1-2',15,11,0.0,7).
-consulta('2015-4-4',11,8,80.50,8).
-consulta('2015-7-25',15,11,0.0,7).
-consulta('2015-9-5',14,12,20.50,6).
-consulta('2016-5-30',12,5,75.50,14).
-consulta('2016-9-5',4,2,15.0,2).
-consulta('2016-7-28',7,10,0.0,3).
-consulta('2017-2-12',8,10,0.0,3).
-consulta('2017-7-26',3,3,0.0,11).
-consulta('2018-6-15',13,6,0.0,12).
-consulta('2018-6-29',10,12,35.0,6).
+consulta("2012-3-12",1,1,55.0,4).
+consulta("2012-10-1",2,1,55.0,5).
+consulta("2014-1-18",5,6,0.0,12).
+consulta("2014-2-7",9,12,20.50,6).
+consulta("2015-1-2",15,11,0.0,7).
+consulta("2015-4-4",11,8,80.50,8).
+consulta("2015-7-25",15,11,0.0,7).
+consulta("2015-9-5",14,12,20.50,6).
+consulta("2016-5-30",12,5,75.50,14).
+consulta("2016-9-5",4,2,15.0,2).
+consulta("2016-7-28",7,10,0.0,3).
+consulta("2017-2-12",8,10,0.0,3).
+consulta("2017-7-26",3,3,0.0,11).
+consulta("2018-6-15",13,6,0.0,12).
+consulta("2018-6-29",10,12,35.0,6).
 
 %--------------------------------------------------------------------------------------------
 % Extensao do predicado solutions: X, Z, Lista -> {V,F} 
@@ -130,7 +132,8 @@ comprimento([H|T],R) :- comprimento(T,L),
 nao(Questao) :- Questao, !, fail.
 nao(Questao).
 
-% Extensao do meta-predicado demo: Q -> {V,F,D}
+% Extensao do meta-predicado demo: Questao,Resposta -> {V,F}
+%                            Resposta = { verdadeiro,falso,desconhecido }
 demo(Questao,verdadeiro) :- Questao.
 demo(Questao,falso) :- -Questao.
 demo(Questao,desconhecido) :- nao(Questao), nao(-Questao).
@@ -259,7 +262,7 @@ utenteCidade(C,R)   :- (solutions((ID,N,I,C),utente(ID,N,I,C),R)).
 
 
 % Extensao do predicado servicoID: ID, Resultado -> {V,F}
-servicoID(ID,R)     :- (solutions((ID,D,I,C),servico(ID,D,I,C),R)).
+servicoID(ID,R)     :- (solutions(servico(ID,D,I,C),servico(ID,D,I,C),[R|_])).
 
 % Extensao do predicado servicoDesc: Descricao, Resultado -> {V,F}
 servicoDesc(D,R)    :- (solutions((ID,D,I,C),servico(ID,D,I,C),R)).
@@ -272,23 +275,23 @@ servicoCidade(C,R)  :- (solutions((ID,D,I,C),servico(ID,D,I,C),R)).
 
 
 % Extensao do predicado consultaData: Data, Resultado -> {V,F}
-consultaData(D,R)   :- (solutions((D,IDu,IDs,C,IDm),consulta(D,IDu,IDs,C,IDm),R)).
+consultaData(D,R)   :- (solutions(consulta(D,IDu,IDs,C,IDm),consulta(D,IDu,IDs,C,IDm),[R|_])).
 
 % Extensao do predicado consultaIDu: IDutente, Resultado -> {V,F}
-consultaIDu(IDu,R)  :- (solutions((D,IDu,IDs,C,IDm),consulta(D,IDu,IDs,C,IDm),R)).
+consultaIDu(IDu,R)  :- (solutions(consulta(D,IDu,IDs,C,IDm),consulta(D,IDu,IDs,C,IDm),[R|_])).
 
 % Extensao do predicado consultaIDs: IDservico, Resultado -> {V,F}
-consultaIDs(IDs,R)  :- (solutions((D,IDu,IDs,C,IDm),consulta(D,IDu,IDs,C,IDm),R)).
+consultaIDs(IDs,R)  :- (solutions(consulta(D,IDu,IDs,C,IDm),consulta(D,IDu,IDs,C,IDm),[R|_])).
 
 % Extensao do predicado consultaCusto: Custo, Resultado -> {V,F}
-consultaCusto(C,R)  :- (solutions((D,IDu,IDs,C,IDm),consulta(D,IDu,IDs,C,IDm),R)).
+consultaCusto(C,R)  :- (solutions(consulta(D,IDu,IDs,C,IDm),consulta(D,IDu,IDs,C,IDm),[R|_])).
 
 % Extensao do predicado consultaIDm: IDmedico, Resultado -> {V,F}
-consultaIDm(IDm,R)  :- (solutions((D,IDu,IDs,C,IDm),consulta(D,IDu,IDs,C,IDm),R)).
+consultaIDm(IDm,R)  :- (solutions(consulta(D,IDu,IDs,C,IDm),consulta(D,IDu,IDs,C,IDm),[R|_])).
 
 
 % Extensao do predicado medicoID: IDmedico, Resultado -> {V,F}
-medicoID(IDm,R) :- (solutions((IDm,N),medico(IDm,N),R)).
+medicoID(IDm,R) :- (solutions(medico(IDm,N),medico(IDm,N),[R|_])).
 
 % Extensao do predicado medicoNome: Nome, Resultado -> {V,F}
 medicoNome(N,R) :- (solutions((IDm,N),medico(IDm,N),R)).
@@ -449,27 +452,27 @@ nomeMedico(IDm,R) :- solutions(N,medico(IDm,N),R).
 % ----> Conhecimento Incerto <---- %
 
 % Cidade do utente 2 desconhecida
-utente(2,'Jose Pedro Morais',50,cid_desconhecida).
+utente(2,"Jose Pedro Morais",50,cid_desconhecida).
 
 % Cidade do utente 5 desconhecida, mas sabendo que não é Braga
-utente(5,'Rui Pereira',27,cid_desconhecida).
--utente(5,'Rui Pereira',27,'Braga').
+utente(5,"Rui Pereira",27,cid_desconhecida).
+-utente(5,"Rui Pereira",27,"Braga").
 
 % Idade do utente 8 desconhecida
-utente(8,'Andre Sales',idade_desc,'Lisboa').
+utente(8,"Andre Sales",idade_desc,"Lisboa").
 
 % Instituicao do servico 4 desconhecida
-servico(4,'Ortopedia',inst_desc,'Braga').
+servico(4,"Ortopedia",inst_desc,"Braga").
 
-% Descricao do servico 6 desconhecida
-servico(6,descricao_desc,'Hospital de Santa Maria','Porto').
+% Descricao do servico 6 desconhecida, sabendo que nao é Urgencia
+servico(6,descricao_desc,"Hospital de Santa Maria","Porto").
+-servico(6,"Urgencia","Hospital de Santa Maria","Porto").
 
 % Custo de consulta desconhecido
-consulta('2011-12-12',1,1,custo_desc,1).
+consulta("2011-12-12",1,1,custo_desc,1).
 
-% Medico de consulta desconhecido, sabendo que nao é o médico 6
-consulta('2013-5-23',6,4,45.0,med_desc).
--consulta('2013-5-23',6,4,45.0,6).
+% Data de consulta desconhecida
+consulta(no_date,6,4,45.0,6).
 
 
 excecao(utente(ID,N,I,_)) :- utente(ID,N,I,cid_desconhecida).
@@ -478,34 +481,35 @@ excecao(servico(IDs,D,_,C)) :- servico(IDs,D,inst_desc,C).
 excecao(servico(IDs,_,I,C)) :- servico(IDs,descricao_desc,I,C).
 excecao(consulta(D,IDu,IDs,C,_)) :- consulta(D,IDu,IDs,C,med_desc).
 excecao(consulta(D,IDu,IDs,_,IDm)) :- consulta(D,IDu,IDs,custo_desc,IDm).
+excecao(consulta(_,IDu,IDs,C,IDm)) :- consulta(no_date,IDu,IDs,C,IDm).
 
 %--------------------------------------------------------------------------------------------
 % ----> Conhecimento Impreciso <---- %
 
 % Idade do utente 6 desconhecida, mas sabendo que está entre 50 e 60
-utente(6,'Rui Vieira',idade_imp,'Povoa de Lanhoso').
+utente(6,"Rui Vieira",idade_imp,"Povoa de Lanhoso").
 
-excecao(utente(6,'Rui Vieira',Idade,'Povoa de Lanhoso')) 
+excecao(utente(6,"Rui Vieira",Idade,"Povoa de Lanhoso")) 
 	:- Idade >= 50,
 	   Idade =< 60.
 
 % Nome do utente 1 podera ter como apelido Oliveira ou Ferreira
-utente(1,nome_imp,60,'Braga').
+utente(1,nome_imp,60,"Braga").
 
-excecao(utente(1,'Pedro Oliveira',60,'Braga')).
-excecao(utente(1,'Pedro Ferreira',60,'Braga')).
+excecao(utente(1,"Pedro Oliveira",60,"Braga")).
+excecao(utente(1,"Pedro Ferreira",60,"Braga")).
 
 % Descricao do servico 11 poderá ser Urgencia, Cardiologia ou Oftalmologia
-servico(11,descricao_imp,'Hospital Lusiadas','Faro').
+servico(11,descricao_imp,"Hospital Lusiadas","Faro").
 
-excecao(servico(11,'Urgencia','Hospital Lusiadas','Faro')).
-excecao(servico(11,'Cardiologia','Hospital Lusiadas','Faro')).
-excecao(servico(11,'Oftalmologia','Hospital Lusiadas','Faro')).
+excecao(servico(11,"Urgencia","Hospital Lusiadas","Faro")).
+excecao(servico(11,"Cardiologia","Hospital Lusiadas","Faro")).
+excecao(servico(11,"Oftalmologia","Hospital Lusiadas","Faro")).
 
 % Custo de uma consulta imprecisa, mas sabendo que está entre 20 e 40
-consulta('2018-12-10',10,12,custo_imp,6).
+consulta("2018-12-10",10,12,custo_imp,6).
 
-excecao(consulta('2018-12-10',10,12,Custo,6))
+excecao(consulta("2018-12-10",10,12,Custo,6))
 	:- Custo >= 20,
 	   Custo =< 40.
 
@@ -515,7 +519,7 @@ excecao(consulta('2018-12-10',10,12,Custo,6))
 % Impossibilidade de saber cidade do utente 14
 nulo(cidade_interdita).
 
-utente(14,'Ana Beatriz de Oliveira',32,cidade_interdita).
+utente(14,"Ana Beatriz de Oliveira",32,cidade_interdita).
 +utente(_,_,_,Cidade) :: (solutions(Cidade, 
 				(utente(14,_,_,Cidade), nao(nulo(Cidade))),S),
 				comprimento(S,N), N==0).
@@ -524,7 +528,7 @@ utente(14,'Ana Beatriz de Oliveira',32,cidade_interdita).
 % Impossibilidade de saber Instituicao do servico 10
 nulo(inst_interdita).
 
-servico(10,'Urgencia',inst_interdita,'Lisboa').
+servico(10,"Urgencia",inst_interdita,"Lisboa").
 +servico(_,_,Inst,_) :: (solutions(Inst, 
 				(servico(10,_,Inst,_), nao(nulo(Inst))),S),
 				comprimento(S,N), N==0).
@@ -532,9 +536,9 @@ servico(10,'Urgencia',inst_interdita,'Lisboa').
 % Impossibilidade de saber Custo de uma consulta
 nulo(custo_interdito).
 
-consulta('2017-8-20',10,12,custo_interdito,6).
+consulta("2017-8-20",10,12,custo_interdito,6).
 +consulta(_,_,_,Custo,_) :: (solutions(Custo, 
-				(consulta('2017-8-20',_,_,Custo,_), nao(nulo(Custo))),S),
+				(consulta("2017-8-20",_,_,Custo,_), nao(nulo(Custo))),S),
 				comprimento(S,N), N==0).
 
 % Impossibilidade de saber nome do medico 15
@@ -545,7 +549,6 @@ medico(15,nome_interdito).
 				(medico(15,Nome), nao(nulo(Nome))),S),
 				comprimento(S,N), N==0).
 
-
 %--------------------------------------------------------------------------------------------
 % Evolução de Conhecimento Imperfeito
 %--------------------------------------------------------------------------------------------
@@ -554,5 +557,159 @@ medico(15,nome_interdito).
 trocar(A,N) :- remover(A), evolucao(N).
 trocar(A,_) :- assert(A), !, fail.
 
-% Transformar conhecimento imperfeito (desconhecido/impreciso) em conhecimento perfeito
+%--------------------------------------------------------------------------------------------
+% --> Transformar conhecimento imperfeito (desconhecido/impreciso) em conhecimento perfeito
 
+% [UTENTES]
+% Adicionar nome a um utente
+conhecer_utente(nome, ID, Nome) :- utenteID(ID, utente(ID,S,I,C)),
+								   atom(S), nao(atom(Nome)),
+								   trocar(utente(ID,S,I,C), utente(ID,Nome,I,C)).
+
+% Adicionar idade a um utente
+conhecer_utente(idade, ID, Idade) :- utenteID(ID, utente(ID,N,S,C)),
+									 atom(S), nao(atom(Idade)),
+									 trocar(utente(ID,N,S,C), utente(ID,N,Idade,C)).
+
+% Adicionar cidade a um utente
+conhecer_utente(cidade, ID, Cidade) :- utenteID(ID, utente(ID,N,I,S)),
+									   atom(S), nao(atom(Cidade)),
+									   trocar(utente(ID,N,I,S), utente(ID,N,I,Cidade)).
+
+% [SERVICOS]
+% Adicionar descricao a um servico
+conhecer_servico(descricao, ID, Desc) :- servicoID(ID, servico(ID,S,I,C)),
+										 atom(S), nao(atom(Desc)),
+										 trocar(servico(ID,S,I,C), servico(ID,Desc,I,C)).
+
+% Adicionar instituicao a um servico
+conhecer_servico(instituicao, ID, Inst) :- servicoID(ID, servico(ID,D,S,C)),
+										   atom(S), nao(atom(Inst)),
+										   trocar(servico(ID,D,S,C), servico(ID,D,Inst,C)).
+
+% Adicionar cidade a um servico
+conhecer_servico(cidade, ID, Cidade) :- servicoID(ID, servico(ID,D,I,S)),
+										atom(S), nao(atom(Cidade)),
+										trocar(servico(ID,D,I,S), servico(ID,D,I,Cidade)).
+
+% [MEDICOS]
+% Adiconar nome a um medico
+conhecer_medico(nome, ID, Nome) :- medicoID(ID, medico(ID,S)),
+								   atom(S), nao(atom(Nome)),
+								   trocar(medico(ID,S), medico(ID,Nome)).
+
+% [CONSULTAS]
+% Adicionar data a uma consulta (Simbolo = no_date, por ex.)
+conhecer_consulta(data, Simbolo, Data) :- consultaData(Simbolo, consulta(Simbolo,IDu,IDs,C,IDm)),
+											atom(Simbolo), nao(atom(Data)),
+											trocar(consulta(Simbolo,IDu,IDs,C,IDm),consulta(Data,IDu,IDs,C,IDm)).
+
+% Adicionar custo a uma consulta (Simbolo = no_custo, por ex.)
+conhecer_consulta(custo, Simbolo, Custo) :- consultaCusto(Simbolo, consulta(D,IDu,IDs,Simbolo,IDm)),
+											atom(Simbolo), nao(atom(Custo)),
+											trocar(consulta(D,IDu,IDs,Simbolo,IDm),consulta(D,IDu,IDs,Custo,IDm)).
+
+%--------------------------------------------------------------------------------------------
+% --> Declarar conhecimento Incerto
+
+% [UTENTES]
+% Nome incerto 
+utente_incerto(nome,ID) :- utenteID(ID,utente(_,N,_,_)), atom(N),
+		       evolucao(excecao(utente(ID,_,I,C)) :- utente(ID,N,I,C)).
+
+% Idade incerta 
+utente_incerto(idade,ID) :- utenteID(ID,utente(_,_,I,_)), atom(I),
+		       evolucao(excecao(utente(ID,N,_,C)) :- utente(ID,N,I,C)).
+
+% Cidade incerta 
+utente_incerto(cidade,ID) :- utenteID(ID,utente(_,_,_,C)), atom(C),
+		       evolucao(excecao(utente(ID,N,I,_)) :- utente(ID,N,I,C)).
+
+% [SERVICOS]
+% Descricao incerta
+servico_incerto(descricao,ID) :- servicoID(ID,servico(_,D,_,_)), atom(D),
+		       evolucao(excecao(servico(ID,_,I,C)) :- servico(ID,D,I,C)).
+
+% Instituicao incerta
+servico_incerto(instituicao,ID) :- servicoID(ID,servico(_,_,I,_)), atom(I),
+		       evolucao(excecao(servico(ID,D,_,C)) :- servico(ID,D,I,C)).
+
+% Cidade incerta
+servico_incerto(cidade,ID) :- servicoID(ID,servico(_,_,_,C)), atom(C),
+		       evolucao(excecao(servico(ID,D,I,_)) :- servico(ID,D,I,C)).
+
+% [MEDICOS]
+% Nome incerto
+medico_incerto(nome,ID) :- medicoID(ID,medico(_,N)), atom(N),
+		       evolucao(excecao(medico(ID,_)) :- medico(ID,N)).
+
+% [CONSULTAS]
+% Data incerta (Simbolo = no_date, por ex.)
+consulta_incerta(data, Simbolo) :- consultaData(Simbolo, consulta(Simbolo,_,_,_,_)), 
+			   atom(Simbolo),
+evolucao(excecao(consulta(_,IDu,IDs,C,IDm)) :- consulta(Simbolo,IDu,IDs,C,IDm)).
+
+% Custo incerto (Simbolo = no_custo, por ex.)
+consulta_incerta(custo, Simbolo) :- consultaCusto(Simbolo, consulta(_,_,_,Simbolo,_)), 
+			   atom(Simbolo),
+evolucao(excecao(consulta(D,IDu,IDs,_,IDm)) :- consulta(D,IDu,IDs,Simbolo,IDm)).
+
+%--------------------------------------------------------------------------------------------
+% --> Declarar conhecimento Impreciso
+
+
+
+%--------------------------------------------------------------------------------------------
+% --> Declarar conhecimento Interdito
+
+% [UTENTES]
+% Interdicao de nome 
+utente_interdito(nome,ID) :- utenteID(ID,utente(_,N,_,_)),
+						evolucao(nulo(N)),
+evolucao(+utente(ID,_,_,_) :: (solutions(Nome,(utente(ID,Nome,_,_),nao(nulo(Nome))),L),comprimento(L,C),C == 0)).
+
+% Interdicao de idade
+utente_interdito(idade,ID) :- utenteID(ID,utente(_,_,I,_)),
+						evolucao(nulo(I)),
+evolucao(+utente(ID,_,_,_) :: (solutions(Idade,(utente(ID,_,Idade,_),nao(nulo(Idade))),L),comprimento(L,C),C == 0)).
+
+% Interdicao de cidade
+utente_interdito(cidade,ID) :- utenteID(ID,utente(_,_,_,C)),
+						evolucao(nulo(C)),
+evolucao(+utente(ID,_,_,_) :: (solutions(Cidade,(utente(ID,_,_,Cidade),nao(nulo(Cidade))),L),comprimento(L,C),C == 0)).
+
+% [SERVICOS]
+% Interdicao de descricao
+servico_interdito(descricao,ID) :- servicoID(ID,servico(_,D,_,_)),
+						evolucao(nulo(D)),
+evolucao(+servico(ID,_,_,_) :: (solutions(Descricao,(servico(ID,Descricao,_,_),nao(nulo(Descricao))),L),comprimento(L,C),C == 0)).
+
+% Interdicao de instituicao
+servico_interdito(instituicao,ID) :- servicoID(ID,servico(_,_,I,_)),
+						evolucao(nulo(I)),
+evolucao(+servico(ID,_,_,_) :: (solutions(Inst,(servico(ID,_,Inst,_),nao(nulo(Inst))),L),comprimento(L,C),C == 0)).
+
+%Interdicao de cidade
+servico_interdito(cidade,ID) :- servicoID(ID,servico(_,_,_,C)),
+						evolucao(nulo(C)),
+evolucao(+servico(ID,_,_,_) :: (solutions(Cidade,(servico(ID,_,_,Cidade),nao(nulo(Cidade))),L),comprimento(L,C),C == 0)).
+
+% [MEDICOS]
+% Interdicao de nome
+medico_interdito(nome,ID) :- medicoID(ID,medico(_,N)),
+						evolucao(nulo(N)),
+evolucao(+medico(ID,_) :: (solutions(Nome,(medico(ID,Nome),nao(nulo(Nome))),L),comprimento(L,C),C == 0)).
+
+% [CONSULTAS]
+% Interdicao de data (Simbolo = no_date, por ex.)
+consulta_interdita(data,Simbolo) :- consultaData(Simbolo, consulta(Simbolo,_,_,_,_)),
+						evolucao(nulo(Simbolo)),
+evolucao(+consulta(Data,_,_,_,_) :: (solutions(Data,(consulta(Data,_,_,_,_),nao(nulo(Data))),L),comprimento(L,C),C == 0)).
+
+% Interdicao de custo (Simbolo = no_custo, por ex.)
+consulta_interdita(custo,Simbolo) :- consultaCusto(Simbolo, consulta(_,_,_,Simbolo,_)),
+						evolucao(nulo(Simbolo)),
+evolucao(+consulta(_,_,_,Custo,_) :: (solutions(Custo,(consulta(_,_,_,Custo,_),nao(nulo(Custo))),L),comprimento(L,C),C == 0)).
+
+% Invariante de insercao de interdiçoes (impede interdiçoes duplicadas)
++nulo(N) :: (solutions(N,nulo(N),R),comprimento(R,C),C==1).
